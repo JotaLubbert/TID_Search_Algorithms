6cf81ccb1f8912f7ff979d4307010e0ce5b7a6fb
JotaLubbert <jlubbert@alumnos.uai.cl>
2026-08-06 14:48:23 -0400

    se inicializa el archivo a_star.rs con las dependencias necesarias.

---
+++ src/a_star.rs
@@ -0,0 +1,6 @@
+use std::collections::BinaryHeap;
+use std::collections::HashMap;
+
+pub fn a_star(map:&[&[bool]]){
+    
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod read_map;
+mod a_star;
 fn main(){
     let mut array: [[bool; 512]; 512] = [[false; 512]; 512];
     read_map::read_map(&mut array, "test_maps/arena2.map");
