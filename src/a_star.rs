a6ed6622998289731b02eb46773e4219b4b8eb02
JotaLubbert <jlubbert@alumnos.uai.cl>
2026-08-07 10:01:52 -0400

    Se avanza en a_star, se declara el binary heap y se permite recibir una función de distancia, en el caso más simple, la euclideana. (se define la función de distancia euclideana).

--- src/a_star.rs
+++ src/a_star.rs
@@ -1,6 +1,20 @@
 use std::collections::BinaryHeap;
 use std::collections::HashMap;
 
-pub fn a_star(map:&[&[bool]]){
-    
+fn euclidean_distance(vec1: &[f64], vec2: &[f64])->f64{
+    if vec1.iter().len() != vec2.iter().len() {
+        panic!("Expected 2 equal size vectors");
+    }
+    let mut res: f64 = 0.0;
+    for (i, val) in vec1.iter().enumerate() {
+        res += (vec2[i] - val).powi(2);
+    }
+    return res;
+}
+
+pub fn a_star<Func>(map:&[&[bool]], distance: Func)
+where Func: Fn(&[f64], &[f64])->f64
+{
+    let mut open: BinaryHeap<(usize, usize)> = BinaryHeap::new();
+    let mut close = HashMap::new();
 }
