69546daa567d9e2a376d20fa1eec78ea06397dab
JotaLubbert <jlubbert@alumnos.uai.cl>
2026-08-07 12:04:23 -0400

    Ya, hasta aquí llego, espero que sea un progreso conciderable

--- src/a_star.rs
+++ src/a_star.rs
@@ -1,20 +1,34 @@
+use std::cmp::Reverse;
 use std::collections::BinaryHeap;
 use std::collections::HashMap;
+type Coords = (u32, u32);
+type Distance = u64;
 
-fn euclidean_distance(vec1: &[f64], vec2: &[f64])->f64{
-    if vec1.iter().len() != vec2.iter().len() {
-        panic!("Expected 2 equal size vectors");
-    }
-    let mut res: f64 = 0.0;
-    for (i, val) in vec1.iter().enumerate() {
-        res += (vec2[i] - val).powi(2);
-    }
-    return res;
+
+fn euclidean_distance_squared(tup1: Coords, tup2: Coords)->u64{
+    return (tup1.0 + tup2.0).pow(2) as u64 +(tup1.1 + tup2.1).pow(2) as u64;
 }
 
-pub fn a_star<Func>(map:&[&[bool]], distance: Func)
-where Func: Fn(&[f64], &[f64])->f64
+fn is_goal(best: &(Reverse<u64>, (u32, u32)))->bool{
+    /* Something happends */
+    return true;
+}
+
+pub fn a_star<Func>(start: Coords, goal: Coords, map:&[&[bool]], type_of_distance: Func) /*Hay que poner el return*/
+where Func: Fn(Coords, Coords)->Distance
 {
-    let mut open: BinaryHeap<(usize, usize)> = BinaryHeap::new();
-    let mut close = HashMap::new();
+    let mut open: BinaryHeap<(Reverse<Distance>, Coords)> = BinaryHeap::new();
+    let mut close: HashMap<Coords, Distance> = HashMap::new();
+    open.push((Reverse(type_of_distance(start, goal)), start));
+    while !open.is_empty() {
+        let best  = open.peek().unwrap();
+        if is_goal(best){
+            return path;
+        }
+        let (val, key) = open.pop().unwrap();
+        close.insert(key, val.0);
+        for /*action to do */{
+            
+        }
+    }
 }
