5e3a6eef0fba539005227754998c3d91cb81969b
JotaLubbert <jlubbert@alumnos.uai.cl>
2026-08-12 11:40:43 -0400

    Se crea una función que crea puntos aleatoreos y verifica que sean caminables. Se crea una función para probar los puntos creados para probar el algoritmo A*

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 mod read_map;
 mod a_star;
+mod random_functions;
 //mod a_estrella;
 mod distances_types;
 fn main(){
     let mut array: [[bool; 512]; 512] = [[false; 512]; 512];
     read_map::read_map(&mut array, "test_maps/arena2.map");
     //print!("{:?}", array[0]);
-    let res = a_star::a_star((0, 0), (5,5), &mut array, distances_types::euclidean_distance);
-    println!("{:?}", res.unwrap().1);
+    //let res = a_star::a_star((0, 0), (5,5), &mut array, distances_types::euclidean_distance);
+    let test_results = random_functions::test_astar(&mut array, 15);
+    for (time, path) in test_results{
+        println!("Time (in miliseconds): {}", time);
+        println!("path: ");
+        println!("{:?}", path);
+    }
 }
