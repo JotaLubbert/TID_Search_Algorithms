c355ab98999f5b91c1377a1aa6f577409664c1c0
JotaLubbert <jlubbert@alumnos.uai.cl>
2026-08-12 16:34:11 -0400

    Se agrega el A* de rust por defecto como comparación. falta implementarlo

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod a_star;
 mod random_functions;
 //mod a_estrella;
 mod distances_types;
+use pathfinding::directed::astar;
 fn main(){
     let mut array: [[bool; 512]; 512] = [[false; 512]; 512];
     read_map::read_map(&mut array, "test_maps/arena2.map");
