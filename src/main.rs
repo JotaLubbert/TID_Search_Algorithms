342070c4fffc7a53b4547383da4a6532c0a4d193
JotaLubbert <jlubbert@alumnos.uai.cl>
2026-08-12 19:12:00 -0400

    descomento la implementación de tata y elimino el camino A* de rust. No lo logré entender, xd

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 mod read_map;
 mod a_star;
 mod random_functions;
-//mod a_estrella;
+mod a_estrella;
 mod distances_types;
-use pathfinding::directed::astar;
 fn main(){
     let mut array: [[bool; 512]; 512] = [[false; 512]; 512];
     read_map::read_map(&mut array, "test_maps/arena2.map");
