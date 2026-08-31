9e329adf98af0874eead0c0f4e61a1dc2d2b9530
JotaLubbert <jlubbert@alumnos.uai.cl>
2026-08-31 11:56:30 -0400

    Se agrega la distancia de manhattan
    Solo se agrega la distancia de manhattan

--- src/distances_types.rs
+++ src/distances_types.rs
@@ -1,6 +1,11 @@
 type Coords = (u32, u32);
 type Distance = f64;
 
-pub fn euclidean_distance(tup1: Coords, tup2: Coords)->Distance{
-    return (((tup1.0 + tup2.0).pow(2) +(tup1.1 + tup2.1).pow(2)) as f64).powf(0.5);
+pub fn euclidean_distance(tup1: Coords, tup2: Coords) -> Distance {
+    return (((tup1.0 + tup2.0).pow(2) + (tup1.1 + tup2.1).pow(2)) as f64).powf(0.5);
+}
+
+
+pub fn manhattan_distance(tup1: Coords, tup2: Coords)->Distance{
+    return (tup1.0 + tup2.0 + tup1.1 + tup2.1) as f64;
 }
