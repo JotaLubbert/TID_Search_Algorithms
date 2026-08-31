f4df77b14c032c3b1bfd02f4d219aa59cb5a4a6e
JotaLubbert <jlubbert@alumnos.uai.cl>
2026-08-31 19:04:59 -0400

    bugfix de la heuristica (sé que se puede hacer solo en una linea, pero así se ve mejor)

--- src/distances_types.rs
+++ src/distances_types.rs
@@ -1,6 +1,15 @@
 type Coords = (u32, u32);
 type Distance = f64;
 
-pub fn euclidean_distance(tup1: Coords, tup2: Coords)->Distance{
-    return (((tup1.0 + tup2.0).pow(2) +(tup1.1 + tup2.1).pow(2)) as f64).powf(0.5);
+pub fn euclidean_distance(tup1: Coords, tup2: Coords) -> Distance {
+    let dx = tup1.0 as f64 - tup2.0 as f64;
+    let dy = tup1.1 as f64 - tup2.1 as f64;
+    return (dx * dx + dy * dy).sqrt();
+}
+
+
+pub fn manhattan_distance(tup1: Coords, tup2: Coords)->Distance{
+    let dx = (tup1.0 as f64 - tup2.0 as f64).abs();
+    let dy = (tup1.1 as f64 - tup2.1 as f64).abs();
+    return dx + dy;
 }
