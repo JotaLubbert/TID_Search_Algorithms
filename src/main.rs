64d7c8263f44adb90d3ccf7edbe031f1338ae96a
JotaLubbert <jlubbert@alumnos.uai.cl>
2026-08-30 22:28:24 -0400

    comit para guardar el branch

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,12 @@
-mod read_map;
 mod a_star;
+mod read_map;
 mod search_functions;
 mod testing_functions;
 //mod a_estrella;
 mod distances_types;
 mod map_visualization;
-fn main(){
+fn main() {
     let mut array: [[bool; 512]; 512] = [[false; 512]; 512];
     read_map::read_map(&mut array, "test_maps/test_small.map");
-    //print!("{:?}", array[0]);
-    //let res = a_star::a_star((0, 0), (5,5), &mut array, distances_types::euclidean_distance);
-    // let test_results = testing_functions::test_astar(&mut array, 15);
-    // for (time, path) in test_results{
-    //     println!("Time: {}ms", time);
-    //     println!("path: ");
-    //     println!("{:?}", path);
-    // }
     testing_functions::test_visualizer(&mut array);
-
 }
